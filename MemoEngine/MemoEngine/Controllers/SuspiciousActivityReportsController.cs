19f3ec4b76bbccadde2e649a28a52dea3a51979e
Park YongJune <redplus@redplus.net>
2024-05-19 23:37:29 +0900

    Scaffolding CRUD

---
+++ MemoEngine/MemoEngine/Controllers/SuspiciousActivityReportsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MemoEngine.Data;
+using MemoEngine.Models;
+
+namespace MemoEngine.Controllers
+{
+    public class SuspiciousActivityReportsController : Controller
+    {
+        private readonly TenantDbContext _context;
+
+        public SuspiciousActivityReportsController(TenantDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SuspiciousActivityReports
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.SuspiciousActivityReports.ToListAsync());
+        }
+
+        // GET: SuspiciousActivityReports/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var suspiciousActivityReport = await _context.SuspiciousActivityReports
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (suspiciousActivityReport == null)
+            {
+                return NotFound();
+            }
+
+            return View(suspiciousActivityReport);
+        }
+
+        // GET: SuspiciousActivityReports/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SuspiciousActivityReports/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ReportCorrectionIndicator,Subject_Multiple,FirstName,MiddleInitial,LastName,AlsoKnownAs,DoingBusinessAs,Occupation,Address,City,State,ZIPCode,Country,DateOfBirth,GovtIssuedID_Type,GovtIssuedID_Number,GovtIssuedID_IssuingState,SSN,ITIN,EIN,AccountNumber,PhoneNumberHome,PhoneNumberWork,EmailAddress,VehicleLicenseNumber,VehicleLicenseState,CasinoAffiliation,BusinessRelationshipStatus,SuspiciousActivityType,SuspiciousActivityStartDate,SuspiciousActivityEndDate,SuspiciousActivityAmount,LawEnforcementContacted,ContactPersonName,ContactPersonPhone,ContactDate,ReportingCasinoTradeName,ReportingCasinoLegalName,CasinoAddress,CasinoCity,CasinoState,CasinoZIPCode,GamingInstitutionType,CasinoEIN,ContactLastName,ContactFirstName,ContactMiddleInitial,ContactTitle,ContactWorkPhone,ReportPreparedDate,NarrativeDescription")] SuspiciousActivityReport suspiciousActivityReport)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(suspiciousActivityReport);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(suspiciousActivityReport);
+        }
+
+        // GET: SuspiciousActivityReports/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var suspiciousActivityReport = await _context.SuspiciousActivityReports.FindAsync(id);
+            if (suspiciousActivityReport == null)
+            {
+                return NotFound();
+            }
+            return View(suspiciousActivityReport);
+        }
+
+        // POST: SuspiciousActivityReports/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ReportCorrectionIndicator,Subject_Multiple,FirstName,MiddleInitial,LastName,AlsoKnownAs,DoingBusinessAs,Occupation,Address,City,State,ZIPCode,Country,DateOfBirth,GovtIssuedID_Type,GovtIssuedID_Number,GovtIssuedID_IssuingState,SSN,ITIN,EIN,AccountNumber,PhoneNumberHome,PhoneNumberWork,EmailAddress,VehicleLicenseNumber,VehicleLicenseState,CasinoAffiliation,BusinessRelationshipStatus,SuspiciousActivityType,SuspiciousActivityStartDate,SuspiciousActivityEndDate,SuspiciousActivityAmount,LawEnforcementContacted,ContactPersonName,ContactPersonPhone,ContactDate,ReportingCasinoTradeName,ReportingCasinoLegalName,CasinoAddress,CasinoCity,CasinoState,CasinoZIPCode,GamingInstitutionType,CasinoEIN,ContactLastName,ContactFirstName,ContactMiddleInitial,ContactTitle,ContactWorkPhone,ReportPreparedDate,NarrativeDescription")] SuspiciousActivityReport suspiciousActivityReport)
+        {
+            if (id != suspiciousActivityReport.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(suspiciousActivityReport);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SuspiciousActivityReportExists(suspiciousActivityReport.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(suspiciousActivityReport);
+        }
+
+        // GET: SuspiciousActivityReports/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var suspiciousActivityReport = await _context.SuspiciousActivityReports
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (suspiciousActivityReport == null)
+            {
+                return NotFound();
+            }
+
+            return View(suspiciousActivityReport);
+        }
+
+        // POST: SuspiciousActivityReports/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var suspiciousActivityReport = await _context.SuspiciousActivityReports.FindAsync(id);
+            if (suspiciousActivityReport != null)
+            {
+                _context.SuspiciousActivityReports.Remove(suspiciousActivityReport);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SuspiciousActivityReportExists(int id)
+        {
+            return _context.SuspiciousActivityReports.Any(e => e.Id == id);
+        }
+    }
+}
--- MemoEngine/MemoEngine/Models/SuspiciousActivityReport.cs
+++ MemoEngine/MemoEngine/Models/SuspiciousActivityReport.cs
@@ -6,7 +6,7 @@ public class SuspiciousActivityReport
     public bool ReportCorrectionIndicator { get; set; } // 보고서 수정 여부
     public bool Subject_Multiple { get; set; } // 여러 명의 대상 여부
     public string? FirstName { get; set; } // 이름
-    public char MiddleInitial { get; set; } // 중간 이니셜
+    public char? MiddleInitial { get; set; } // 중간 이니셜
     public string? LastName { get; set; } // 성
     public string? AlsoKnownAs { get; set; } // 다른 이름 (AKA)
     public string? DoingBusinessAs { get; set; } // 사업명 (DBA)
@@ -16,7 +16,7 @@ public class SuspiciousActivityReport
     public string? State { get; set; } // 주/도
     public string? ZIPCode { get; set; } // 우편번호
     public string? Country { get; set; } // 국가
-    public DateTime DateOfBirth { get; set; } // 생년월일
+    public DateTime? DateOfBirth { get; set; } // 생년월일
     public string? GovtIssuedID_Type { get; set; } // 정부 발행 신분증 종류
     public string? GovtIssuedID_Number { get; set; } // 정부 발행 신분증 번호
     public string? GovtIssuedID_IssuingState { get; set; } // 신분증 발행 주/도
@@ -32,13 +32,13 @@ public class SuspiciousActivityReport
     public string? CasinoAffiliation { get; set; } // 카지노 연관 (고객, 대리인 등)
     public string? BusinessRelationshipStatus { get; set; } // 사업 관계 상태
     public string? SuspiciousActivityType { get; set; } // 의심스러운 활동 유형
-    public DateTime SuspiciousActivityStartDate { get; set; } // 의심스러운 활동 시작일
-    public DateTime SuspiciousActivityEndDate { get; set; } // 의심스러운 활동 종료일
-    public decimal SuspiciousActivityAmount { get; set; } // 의심스러운 활동 금액
+    public DateTime? SuspiciousActivityStartDate { get; set; } // 의심스러운 활동 시작일
+    public DateTime? SuspiciousActivityEndDate { get; set; } // 의심스러운 활동 종료일
+    public decimal? SuspiciousActivityAmount { get; set; } // 의심스러운 활동 금액
     public string? LawEnforcementContacted { get; set; } // 연락한 법집행 기관
     public string? ContactPersonName { get; set; } // 연락 담당자 이름
     public string? ContactPersonPhone { get; set; } // 연락 담당자 전화번호
-    public DateTime ContactDate { get; set; } // 연락 날짜
+    public DateTime? ContactDate { get; set; } // 연락 날짜
     public string? ReportingCasinoTradeName { get; set; } // 보고하는 카지노 상호명
     public string? ReportingCasinoLegalName { get; set; } // 보고하는 카지노 법적 명칭
     public string? CasinoAddress { get; set; } // 카지노 주소
@@ -49,9 +49,9 @@ public class SuspiciousActivityReport
     public string? CasinoEIN { get; set; } // 카지노 고용주 식별 번호 (EIN)
     public string? ContactLastName { get; set; } // 담당자 성
     public string? ContactFirstName { get; set; } // 담당자 이름
-    public char ContactMiddleInitial { get; set; } // 담당자 중간 이니셜
+    public char? ContactMiddleInitial { get; set; } // 담당자 중간 이니셜
     public string? ContactTitle { get; set; } // 담당자 직함
     public string? ContactWorkPhone { get; set; } // 담당자 직장 전화번호
-    public DateTime ReportPreparedDate { get; set; } // 보고서 작성일
+    public DateTime? ReportPreparedDate { get; set; } // 보고서 작성일
     public string? NarrativeDescription { get; set; } // 설명 내용
 }
